233a86104b64c6696e8e2ffc8a80a5ce7fae2ba0
Martijn Faber <martijn@fabtronics.nl>
2021-06-09 20:22:57 +0200

    feat: add websockets and globe

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,58 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+  <div class="background">
+    <h1 class="countdown">{{ remaining }}</h1>
   </div>
 </template>
 
+<style lang="scss" scoped>
+.background {
+  width: 100vw;
+  height: 100vh;
+  background: url("../assets/globe.svg");
+  background-repeat: no-repeat;
+  background-size: 80% 80%;
+  background-position: center;
+
+  display: grid;
+  place-items: center;
+
+  .countdown {
+    font-size: 200px;
+    text-shadow: 8px 8px 30px #000000;
+  }
+}
+</style>
+
 <script lang="ts">
 import { defineComponent } from "vue";
-import HelloWorld from "@/components/HelloWorld.vue"; // @ is an alias to /src
+import * as io from "socket.io-client";
+import dayjs from "@/utils/day";
+import config from "@/config/config";
 
 export default defineComponent({
   name: "Home",
-  components: {
-    HelloWorld,
+  components: {},
+  data() {
+    return {
+      remaining: null as any,
+    };
+  },
+  methods: {
+    getRemaining() {
+      const socket = io(config.wss.url);
+
+      socket.on("connect", () => {
+        console.log("wss connected");
+
+        socket.on("remaining", ({ data }: any) => {
+          console.log(dayjs.duration(data));
+          this.remaining = new Date(data * 1000).toISOString().substr(11, 8); //format seconds to hh:mm:ss string
+        });
+      });
+    },
+  },
+  created() {
+    this.getRemaining();
   },
 });
 </script>
